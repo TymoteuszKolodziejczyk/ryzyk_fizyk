ecbc55283359376721e40d3575db7158842ecab9
Tymoteusz Kołodziejczyk <Tymoteusz.Kolodziejczyk@tm1.edu.pl>
2025-04-24 10:10:48 +0200

    sdad

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+	$conn =  mysqli_connect("localhost", "root", "", "ryzyk_fizyk");
+?>
+
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ryzyk fizyk</title>
+    <link rel="stylesheet" href="styles.css" type="text/css">
+    <link href="files/logo.png" rel="icon">
+</head>
+<body>
+    
+</body>
+</html>
+
+<?php
+	mysqli_close($conn);
+?>
